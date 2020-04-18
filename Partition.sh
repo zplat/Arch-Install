41e25a0ca9535ff1a1c1494759c4870eed47f446
zplat <5zero.6cool@gmail.com>
2020-04-18 19:39:49 +0100

    Create Partition.sh
    Script to setup partition as btrfs partition.

---
+++ Partition.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env sh
+
+# set url for github download
+Site=https://raw.githubusercontent.com
+Owner=zplat
+Repository=Arch-Install/master
+Script=Intall.sh
+setup-url=$Site/$Owner/$Repositories/$Script
+
+# Encrypt disk/partition
+sryptsetup --hash=sha512 --cipher=two-fish-xts-plain64 --key-size=512 -i 30000 luksFormat /dev/sda5
+
+# open btrfs container 
+cryptsetup --allow-discards --persistent open /dev/sda5 btrfs-system
+
+# Format both boot and root partition
+mkfs.vfat -F32 /dev/sdc1
+mkfs.btrfs -L btrfs /dev/mapper/btrfs-system
+
+# Create btrfs subvolumes 
+mount /dev/mapper/btrfs-system /mnt
+btrfs  subvolume create /mnt/root
+btrfs  subvolume create /mnt/root
+btrfs  subvolume create /mnt/root
+
+umount /mnt
+
+mount -o subvol=root,ssd,compress=lzo,discard /dev/mapper/btrfs-system /mnt
+mkdir /mnt/{boot,home,swap}
+mount -o subvol=home,ssd,compress=lzo,discard /dev/mapper/btrfs-system /mnt/home
+mount -o subvol=swap,ssd,discard /dev/mapper/btrfs-system /mnt/swap
+
+# create the swap
+truncate -s 0 /mnt/swap/swapfile
+chattr +C /mnt/swap/swapfile
+
+dd if=/dev/zero of=/mnt/swap/swapfile bs=1M count=8192 status=progress
+
+chmod 600  /mnt/swap/swapfile
+mkswap /mnt/swap/swapfile
+swapon /mnt/swap/swapfile
+
+# mount boot volume
+mount /dev/sdc1  /mnt/boot
+
+# installation 
+pacstrap /mnt base base-devel git btrfs-progs vim efibootmgr zsh zsh-completions linux linux-firmware networkmanager
+
+# generate fstab
+genfstab -U /mnt > /mnt/etc/fstab
+
+# log into chroot
+curl $setup-url > /mnt/shell.sh
+
+
+
+
+
+
+
